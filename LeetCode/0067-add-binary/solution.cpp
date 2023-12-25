82f41196a3c87cab5bcdfca3c9e395316e719c22
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2023-12-25 23:18:00 +0000

    Sync LeetCode submission - Add Binary - Runtime - 3 ms (17.93%), Memory - 8.8 MB (80.16%)

---
+++ LeetCode/0067-add-binary/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int c = 0;
+        string ans = "";
+        int i = a.length() - 1, j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || c == 1) {
+            int sum = c;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            c = sum / 2;
+            ans = to_string(sum % 2) + ans;
+        }
+
+        return  ans;
+    }
+};
+
