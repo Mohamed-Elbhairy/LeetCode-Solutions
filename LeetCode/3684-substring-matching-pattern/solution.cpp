f0b6f438a83aedc4edb73b7a9f8509c76240ed36
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2025-01-04 16:23:49 +0000

    Sync LeetCode submission - Substring Matching Pattern - Runtime - 0 ms (100.00%), Memory - 9 MB (67.56%)

---
+++ LeetCode/3684-substring-matching-pattern/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasMatch(string s, string p) {
+        int idx= p.find('*');
+        string pre = p.substr(0,idx);
+        string suff = p.substr(idx+1);
+        for(int i =0;i+pre.size()<=s.size();i++){
+            if(s.substr(i,pre.size())==pre){
+                if(s.find(suff, i+pre.size())!=-1)return true;
+            }
+        }
+        return 0;
+    }
+};
