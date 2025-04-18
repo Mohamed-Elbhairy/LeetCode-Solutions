4d8b76f277250d5e2421adf50b35b87ba375973c
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2025-04-18 19:56:02 +0000

    Sync LeetCode submission - Longest Common Subsequence - Runtime - 19 ms (87.70%), Memory - 12.9 MB (85.82%)

---
+++ LeetCode/1250-longest-common-subsequence/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int dp[1001][1001];
+        memset(dp, 0, sizeof dp);
+        int n = text1.size();
+        int m = text2.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; ++j) {
+                if (text1[i] == text2[j]) {
+                    if (i > 0 and j > 0)
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    else
+                        dp[i][j] = 1;
+                } else {
+                    if (i and j)
+                        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    else if (i)
+                        dp[i][j] = dp[i - 1][j];
+                    else if (j)
+                        dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
