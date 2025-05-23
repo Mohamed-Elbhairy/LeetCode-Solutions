698eed2a0948838a7a465dc06df894d6c4876907
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2025-05-23 09:37:34 +0000

    Sync LeetCode submission - Find the Maximum Sum of Node Values - Runtime - 52 ms (17.50%), Memory - 150.6 MB (12.13%)

---
+++ LeetCode/3307-find-the-maximum-sum-of-node-values/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    using ll = long long;
+    vector<vector<ll>> dp;
+    ll rec(int idx, bool X, int k, vector<int>& v) {
+        int n = v.size();
+        if (idx == n)
+            return (X == 0) ? 0 : -1e15;
+        ll& ret = dp[idx][X];
+        if (~ret)
+            return ret;
+        ret = rec(idx + 1, X, k, v) + v[idx];
+        ret = max(rec(idx + 1, X ^ 1, k, v) + (v[idx] ^ k), ret);
+        return ret;
+    }
+    long long maximumValueSum(vector<int>& v, int k,
+                              vector<vector<int>>& edges) {
+        dp = vector<vector<ll>>(v.size() + 1, vector<ll>(3, -1));
+        return rec(0, 0, k, v);
+    }
+};
