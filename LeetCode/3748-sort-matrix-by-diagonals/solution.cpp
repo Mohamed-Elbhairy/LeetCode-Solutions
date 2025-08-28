5fbe743382be742f0a41202fbc5dc0d8ec8b1838
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2025-08-28 11:55:15 +0000

    Sync LeetCode submission - Sort Matrix by Diagonals - Runtime - 7 ms (71.58%), Memory - 43.5 MB (61.02%)

---
+++ LeetCode/3748-sort-matrix-by-diagonals/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> v;
+        for (int i = 0; i < n; ++i) {
+            if (i == 0)
+                continue;
+            int ni = 0, nj = i;
+            while (ni < n && nj < n) {
+
+                v.push_back(grid[ni++][nj++]);
+            }
+            sort(v.rbegin(), v.rend());
+            ni = 0, nj = i;
+            while (ni < n && nj < n) {
+                grid[ni++][nj++] = v.back();
+                v.pop_back();
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int ni = i, nj = 0;
+            while (ni < n && nj < n) {
+                v.push_back(grid[ni++][nj++]);
+            }
+            sort(v.begin(), v.end());
+            ni = i, nj = 0;
+            while (ni < n && nj < n) {
+                grid[ni++][nj++] = v.back();
+                v.pop_back();
+            }
+        }
+
+        return grid;
+    }
+};
