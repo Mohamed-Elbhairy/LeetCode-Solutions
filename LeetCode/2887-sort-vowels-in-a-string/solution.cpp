2367744f0853087913d9f064736e96439b7dd637
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2025-09-11 11:51:08 +0000

    Sync LeetCode submission - Sort Vowels in a String - Runtime - 0 ms (100.00%), Memory - 14.9 MB (96.90%)

---
+++ LeetCode/2887-sort-vowels-in-a-string/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string sortVowels(string& s) {
+        int freq[255] = {0};
+        const char vowels[] = {'A', 'E', 'I', 'O', 'U',
+                               'a', 'e', 'i', 'o', 'u'};
+
+        int idx = 0;
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            freq[s[i]]++;
+        }
+        bool IsVowel[255] = {0};
+        for (int i = 0; i < 10; ++i)
+            IsVowel[vowels[i]] = true;
+        for (int i = 0; i < n; ++i) {
+            bool Ok = IsVowel[s[i]] ;
+            if (Ok) {
+                while (idx < 10 && !freq[vowels[idx]])
+                    ++idx;
+                if (idx == 10)
+                    break;
+                s[i] = vowels[idx];
+                freq[vowels[idx]]--;
+            }
+        }
+        return s;
+    }
+};
