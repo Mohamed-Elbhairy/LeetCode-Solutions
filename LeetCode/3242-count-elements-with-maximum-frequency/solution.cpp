ebcfb3bffff1eaff8a7a6309b542ee4e003093a5
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2025-09-22 01:10:34 +0000

    Sync LeetCode submission - Count Elements With Maximum Frequency - Runtime - 0 ms (100.00%), Memory - 23.1 MB (90.75%)

--- LeetCode/3242-count-elements-with-maximum-frequency/solution.cpp
+++ LeetCode/3242-count-elements-with-maximum-frequency/solution.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-       vector<int>freq(101,0);
-       for(auto i:nums)freq[i]++;
-       int mx=*max_element(freq.begin(),freq.end()); 
-       int ans=0;
-       for(auto i:nums)if(freq[i]==mx)ans++;
-       return ans;
+        int freq[101] = {0};
+        int mx = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            freq[nums[i]]++;
+            mx = max(mx, freq[nums[i]]);
+        }
+        int ans = 0;
+        for (int i = 0; i <= 100; ++i) {
+            freq[i] == mx ? ans++ : ans;
+        }
+        return ans*mx;
     }
 };
