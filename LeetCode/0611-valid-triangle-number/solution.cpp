6b3511b77f1c670982d6e4ce1986a9198ca93372
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2025-09-26 10:41:25 +0000

    Sync LeetCode submission - Valid Triangle Number - Runtime - 171 ms (14.94%), Memory - 16.7 MB (45.87%)

---
+++ LeetCode/0611-valid-triangle-number/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int sum = 0;
+        sort(nums.begin() , nums.end());
+        for (int i = 0 ; i < nums.size() ; i++)
+            for (int j = i+1 ; j < nums.size() ; j++)
+                sum+=((nums.size() - (j+1) )- 
+                (nums.end() - lower_bound(nums.begin() + j + 1 , nums.end() , nums[i] + nums[j])));
+        return sum;
+    }
+};
