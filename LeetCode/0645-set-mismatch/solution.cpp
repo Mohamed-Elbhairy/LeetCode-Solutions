7d2b82a7f569999125f1eccf932800b840914995
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2025-11-21 12:30:15 +0000

    Sync LeetCode submission - Set Mismatch - Runtime - 0 ms (100.00%), Memory - 26.3 MB (46.47%)

---
+++ LeetCode/0645-set-mismatch/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> ind(nums.size() + 1, -1);
+        vector<int> ret;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            if (ind[nums[i]] != -1) {
+                ret.push_back(nums[i]);
+                continue;
+            }
+            ind[nums[i]] = i;
+        }
+        for (int i = 1; i <= n; ++i) {
+            if (ind[i] == -1) {
+                ret.push_back(i);
+                break;
+            }
+        }
+        return ret;
+    }
+};
