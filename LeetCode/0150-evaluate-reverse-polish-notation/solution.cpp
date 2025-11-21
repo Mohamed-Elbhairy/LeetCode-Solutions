bbe92290388421c91584a2eb5750f9eed77707c8
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2025-11-21 17:14:32 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation - Runtime - 0 ms (100.00%), Memory - 17.2 MB (12.98%)

---
+++ LeetCode/0150-evaluate-reverse-polish-notation/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int calc(int a, int b, string c) {
+        if (c == "+")
+            return a + b;
+        else if (c == "-")
+            return a - b;
+        else if (c == "*")
+            return a * b;
+        else
+            return a / b;
+    }
+    int evalRPN(vector<string>& tokens) {
+        string operators = "/+-*";
+        stack<int> st;
+        int ans = -1;
+        for (int i = 0; i < tokens.size(); ++i) {
+            if (operators.find(tokens[i]) != string::npos) {
+                int b = (st.top());
+                st.pop();
+                int a = (st.top());
+                st.pop();
+                st.push(calc(a, b,tokens[i]));
+            }
+            else {
+                st.push(stoi(tokens[i]));
+            }
+        }
+        return st.top();
+    }
+};
