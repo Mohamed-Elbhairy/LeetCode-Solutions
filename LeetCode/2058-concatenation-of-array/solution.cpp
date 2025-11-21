3624a7e6ec5d710bdde15631a36eec7e90a47b42
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2025-11-21 12:20:51 +0000

    Sync LeetCode submission - Concatenation of Array - Runtime - 0 ms (100.00%), Memory - 17.1 MB (28.11%)

--- LeetCode/2058-concatenation-of-array/solution.cpp
+++ LeetCode/2058-concatenation-of-array/solution.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n = nums.size();
-
+        nums.resize(2 * n);
         for (int i = 0; i < n; ++i) {
-            nums.push_back(nums[i]);
+            nums[i + n] = nums[i];
         }
         return nums;
     }
