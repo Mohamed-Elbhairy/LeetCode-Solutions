41f31c84066e46163a2d7bf0b125bbde7d67c43a
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2025-11-25 09:15:27 +0000

    Sync LeetCode submission - Smallest Integer Divisible by K - Runtime - 0 ms (100.00%), Memory - 7.7 MB (80.55%)

--- LeetCode/1064-smallest-integer-divisible-by-k/solution.cpp
+++ LeetCode/1064-smallest-integer-divisible-by-k/solution.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int smallestRepunitDivByK(int k) {
-        int rem = 0;
-        int cur = 10;
-        rem = 1 % k;
-        if (rem == 0)
-            return 1;
-        if(k%2 == 0)
-            return -1;
-        for (int i = 2; i <= k; ++i) {
-            rem = (cur + rem) % k;
-            cur *= 10;
-            cur %= k;
-            if (rem == 0)
+        int rem =0 ;
+        int cur = 10 ;
+        rem =1%k; 
+        if(rem == 0)
+            return 1 ;
+        for(int i = 2;i<=k;++i){
+            rem = (cur+rem)%k;
+            cur*=10;
+            cur%=k;
+            if(rem == 0)
                 return i;
         }
-        return -1;
+        return -1 ;
     }
 };
