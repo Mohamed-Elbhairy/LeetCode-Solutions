8d5712c66953c3e6d55d866d1779129f4f09310e
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2026-05-11 10:18:13 +0000

    Sync LeetCode submission - Separate the Digits in an Array - Runtime - 9 ms (17.57%), Memory - 16.2 MB (16.60%)

---
+++ LeetCode/2639-separate-the-digits-in-an-array/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> ret;
+        for (int i : nums) {
+            vector<int> v1;
+            while (i) {
+                v1.push_back(i % 10);
+                i /= 10;
+            }
+            reverse(v1.begin(), v1.end());
+            for (int j : v1)
+                ret.push_back(j);
+        }
+        return ret;
+    }
+};
