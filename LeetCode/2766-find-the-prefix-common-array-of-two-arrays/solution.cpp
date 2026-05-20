0b828bbfb2cd35433f4b749afed6f62d97cef950
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2026-05-20 16:26:54 +0000

    Sync LeetCode submission - Find the Prefix Common Array of Two Arrays - Runtime - 0 ms (100.00%), Memory - 86.4 MB (52.56%)

---
+++ LeetCode/2766-find-the-prefix-common-array-of-two-arrays/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+
+        vector<int> freq(n + 1), C;
+        int cnt = 0;
+
+        for (int i = 0; i < n; ++i) {
+
+            freq[A[i]]++;
+            freq[B[i]]++;
+
+            if (A[i] == B[i]) {
+                ++cnt;
+            } else {
+                if (freq[A[i]] == 2)
+                    ++cnt;
+                if (freq[B[i]] == 2)
+                    ++cnt;
+            }
+
+            C.push_back(cnt);
+        }
+
+        return C;
+    }
+};
