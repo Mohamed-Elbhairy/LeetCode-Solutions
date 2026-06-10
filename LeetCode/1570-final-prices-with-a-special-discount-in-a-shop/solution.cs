9dd948b9df0b3768ccfd19a6415bb00f963738e4
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2026-06-10 17:39:14 +0000

    Sync LeetCode submission - Final Prices With a Special Discount in a Shop - Runtime - 16 ms (6.55%), Memory - 48.4 MB (6.55%)

---
+++ LeetCode/1570-final-prices-with-a-special-discount-in-a-shop/solution.cs
@@ -0,0 +1,36 @@
+public class Solution
+{
+    public List<int> NextSmaller(int[] arr, int n)
+    {
+        var ret = new List<int> ();
+        for(int i = 0; i < n; ++i)
+        {
+            ret.Add(-1);
+        }
+        var stack = new Stack<int>();
+        stack.Push(n - 1);
+        for(int i =n-2;i>=0; --i)
+        {
+            while (stack.Count > 0 && arr[stack.Peek()] > arr[i])
+            {
+                stack.Pop();
+            }
+            if (stack.Count > 0)
+            {
+                ret[i] = stack.Peek();
+            }
+            stack.Push(i);
+        }
+        return ret;
+    }
+    public int[] FinalPrices(int[] prices)
+    {
+        var ans = new int[prices.Length];
+        var nextSmaller = NextSmaller(prices, prices.Length);
+        for (int i = 0; i < prices.Length; ++i)
+        {
+            ans[i] = prices[i] - (nextSmaller[i] == -1 ? 0 : prices[nextSmaller[i]]);
+        }
+        return ans;
+    }
+}
