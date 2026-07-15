73bc8bde5435cef94a521d27a892bec35a204bca
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2026-07-15 08:55:58 +0000

    Sync LeetCode submission - GCD of Odd and Even Sums - Runtime - 2 ms (11.92%), Memory - 8.6 MB (48.56%)

--- LeetCode/3995-gcd-of-odd-and-even-sums/solution.cpp
+++ LeetCode/3995-gcd-of-odd-and-even-sums/solution.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int a = 0;
-        int b = 0;
-        int cnt1 = 0;
-        int cnt2 = 0;
-        for (int i = 1;; ++i) {
-            if (i & 1 && cnt1 < n)
-                a += i, ++cnt1;
-            if (~i & 1 && cnt2 < n)
-                b += i, ++cnt2;
-            if (cnt1 == n and cnt2 == n)
-                break;
-        }
-        return __gcd(a, b);
+        
+        return gcd(n*n, n*(n+1));
     }
-}
-
-;
+};
