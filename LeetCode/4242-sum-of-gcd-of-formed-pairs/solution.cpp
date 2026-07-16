6aef406eea66824a2bb5159a4f8774493d876580
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2026-07-16 14:30:02 +0000

    Sync LeetCode submission - Sum of GCD of Formed Pairs - Runtime - 75 ms (49.09%), Memory - 148.8 MB (91.92%)

---
+++ LeetCode/4242-sum-of-gcd-of-formed-pairs/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long gcdSum(vector<int>& nums) {
+        int mx = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            mx = max(mx, nums[i]);
+            nums[i] = gcd(mx, nums[i]);
+        }
+        sort(nums.begin(), nums.end());
+        int l = 0, r = nums.size() - 1;
+        long long ret = 0;
+        while (l < r) {
+            ret += gcd(nums[l], nums[r]);
+            ++l, --r;
+        }
+        return ret;
+    }
+};
