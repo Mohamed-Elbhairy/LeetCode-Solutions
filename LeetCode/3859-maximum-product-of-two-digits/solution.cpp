aa40d58d25e21137207e6612fc0b6709240fe27e
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2026-07-25 17:03:32 +0000

    Sync LeetCode submission - Maximum Product of Two Digits - Runtime - 0 ms (100.00%), Memory - 8.7 MB (65.46%)

---
+++ LeetCode/3859-maximum-product-of-two-digits/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int d1 = 0, d2 = 0;
+        while (n) {
+            int d = n % 10;
+            if (d1 < d) {
+                d2 = d1;
+                d1 = d;
+            } else if (d2 < d) {
+                d2 = d;
+            }
+            n /= 10;
+        }
+        return d1 * d2;
+    }
+};
