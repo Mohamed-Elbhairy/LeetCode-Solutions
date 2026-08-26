c9db246cdd3f165d245f2206af97e1559aee884d
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2026-08-26 22:28:05 +0000

    Sync LeetCode submission - Find the Difference of Two Arrays - Runtime - 39 ms (10.83%), Memory - 40.7 MB (6.49%)

---
+++ LeetCode/1392-find-the-difference-of-two-arrays/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        vector<vector<int>> ret(2);
+        unordered_map<int,bool> freq1, freq2;
+        for (int i : nums1)
+            freq1[i] = 1;
+        for (int i : nums2)
+            freq2[i] = 1;
+        for (int i : nums2)
+            if (!freq1[i])
+                ret[1].push_back(i), freq1[i] = 1;
+        for (int i : nums1)
+            if (!freq2[i])
+                ret[0].push_back(i), freq2[i] = 1;
+        return ret;
+    }
+};
