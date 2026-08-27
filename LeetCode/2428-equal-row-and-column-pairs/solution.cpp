1b0569157ebc4518ba48c6bff11982de3462f8d4
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2026-08-27 12:43:40 +0000

    Sync LeetCode submission - Equal Row and Column Pairs - Runtime - 22 ms (45.01%), Memory - 36.9 MB (53.37%)

---
+++ LeetCode/2428-equal-row-and-column-pairs/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+
+        map<vector<int>, int> freq;
+
+        int n = grid.size();
+
+        for (int i = 0; i < n; ++i)
+            freq[grid[i]]++;
+
+        int ret = 0;
+
+        for (int j = 0; j < n; ++j) {
+            vector<int> vec;
+            for (int i = 0; i < n; ++i) {
+                vec.push_back(grid[i][j]);
+            }
+            ret += freq[vec];
+        }
+        return ret;
+    }
+};
