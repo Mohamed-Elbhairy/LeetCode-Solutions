9d53f0ed1f311945f551642f5e6d726a350bc251
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2026-08-30 12:30:21 +0000

    Sync LeetCode submission - Removing Minimum and Maximum From Array - Runtime - 0 ms (100.00%), Memory - 91.9 MB (86.84%)

---
+++ LeetCode/2212-removing-minimum-and-maximum-from-array/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        if (nums.size() == 1)
+            return 1;
+        int n = nums.size();
+        int mxPos = 0, mnPos = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > nums[mxPos]){
+                mxPos = i;
+            }
+            if (nums[i] < nums[mnPos]){
+                mnPos = i;
+            }
+        }
+        int mn = min(mnPos,mxPos);
+        int mx = max(mnPos,mxPos);
+        --n;
+        return min({n-mn+1 , mx+1 , (n-mx+1) + (mn+1)});
+    }
+};
