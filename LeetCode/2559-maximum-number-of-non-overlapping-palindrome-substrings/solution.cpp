dba996908a84b0935ce5c31c7113c89f39159cf9
Mohamed-Elbhairy <143911775+Mohamed-Elbhairy@users.noreply.github.com>
2026-09-15 16:04:10 +0000

    Sync LeetCode submission - Maximum Number of Non-overlapping Palindrome Substrings - Runtime - 195 ms (40.31%), Memory - 19.6 MB (35.71%)

---
+++ LeetCode/2559-maximum-number-of-non-overlapping-palindrome-substrings/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxPalindromes(string s, int k) {
+        int n = s.size();
+        vector<vector<bool>> is_pal(n, vector<bool>(n, true));
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                is_pal[i][j] = (s[i] == s[j]) && is_pal[i + 1][j - 1];
+            }
+        }
+        vector<int> dp(n + 1, 0);
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = dp[i - 1];
+            for (int j = 0; j <= i - k; ++j) {
+                if (is_pal[j][i - 1]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
